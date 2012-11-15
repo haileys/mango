3174991df01714be79f9c982be7d31f4f584acf0
Charlie Somerville <charlie@charliesomerville.com>
2012-11-15 23:37:40 +1100

    dual repl/interpreter

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 import System.IO
+import System.Environment
 import Control.Monad
 import qualified Mango.Reader as R
 import Mango.Eval
@@ -7,18 +8,42 @@ import Mango.Prelude
 import Mango.Exception
 import Data.Map (fromList)
 
-replLine :: Scope -> IO ()
+protect comp = do
+    catchMangoError print comp
+
 replLine scope = do
     putStr ">> "
     hFlush stdout
     code <- getLine
-    catchMangoError print $ do
+    protect $ do
         val <- evalMany scope $ R.read "(stdin)" code
         putStrLn $ "=> " ++ show val
 
-main :: IO ()
-main = do
+banner =
+    "    _ __ ___   __ _ _ __   __ _  ___    \n" ++
+    "   | '_ ` _ \\ / _` | '_ \\ / _` |/ _ \\  \n" ++
+    "   | | | | | | (_| | | | | (_| | (_) | \n" ++
+    "   |_| |_| |_|\\__,_|_| |_|\\__, |\\___/  \n" ++
+    "                           __/ |       \n" ++
+    "                          |___/        "
+
+repl scope = do
+    putStrLn banner
+    forever $ replLine scope
+
+run scope (source:argv) = protect $ do
+    void $ R.readFile source >>= evalMany scope
+
+createGlobalScope = do
     globalScope <- initPrelude
     exprs <- R.readFile "prelude.mang"
     evalMany globalScope exprs
-    forever $ replLine globalScope
+    return globalScope
+
+main :: IO ()
+main = do
+    scope <- createGlobalScope
+    args <- getArgs
+    if null args
+        then repl scope
+        else run scope args
