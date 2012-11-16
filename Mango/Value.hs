a282e6534183e965f9bf31032d647772e4af2312
Charlie Somerville <charlie@charliesomerville.com>
2012-11-16 13:48:21 +1100

    MangoValue instances Eq

--- Mango/Value.hs
+++ Mango/Value.hs
@@ -25,6 +25,22 @@ instance Show MangoValue where
     show (MangoQuote    val)    = '\'' : show val
     show MangoTrue              = "#t"
 
+instance Eq MangoValue where
+    (==) (MangoList     xs) (MangoList      ys) = xs == ys
+    (==) (MangoNumber   a)  (MangoNumber    b)  = a == b
+    (==) (MangoSymbol   a)  (MangoSymbol    b)  = a == b
+    (==) (MangoString   a)  (MangoString    b)  = a == b
+    -- functions in haskell are not instances of Eq so we can't check equality
+    (==) (MangoFunction _)  (MangoFunction  _)  = False
+    (==) (MangoSpecial  _)  (MangoSpecial   _)  = False
+    (==) (MangoQuote    a)  (MangoQuote     b)  = a == b
+    (==) MangoTrue          MangoTrue           = True
+    (==) _                  _                   = False
+
 isTruthy :: MangoValue -> Bool
 isTruthy (MangoList []) = False
 isTruthy _              = True
+
+fromBool :: Bool -> MangoValue
+fromBool True   = MangoTrue
+fromBool False  = MangoList []
